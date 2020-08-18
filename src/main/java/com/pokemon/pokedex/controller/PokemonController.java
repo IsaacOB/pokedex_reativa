8c3e0fe0efac865e00610fb92311a692479bf991
Isaac <isaac.benites@hotmail.com>
2020-08-17 22:22:25 -0300

    Fazendo CRUD básico

--- src/main/java/com/pokemon/pokedex/controller/PokemonController.java
+++ src/main/java/com/pokemon/pokedex/controller/PokemonController.java
@@ -2,12 +2,13 @@ package com.pokemon.pokedex.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,16 +35,41 @@ public class PokemonController {
 
 	@GetMapping("/{id}")
 	public Mono<ResponseEntity<Pokemon>> getPokemon(@PathVariable String id) {
-		return repository.findById(id)
-						 .map(pokemon -> ResponseEntity.ok(pokemon))
-						 .defaultIfEmpty(ResponseEntity.notFound().build());
+		return repository.findById(id).map(pokemon -> ResponseEntity.ok(pokemon))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
-	
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Mono<Pokemon> savePokemon(@RequestBody Pokemon pokemon){
+	public Mono<Pokemon> savePokemon(@RequestBody Pokemon pokemon) {
 		return repository.save(pokemon);
+	}
+
+	@PutMapping("{id}")
+	public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable(value = "id") String id,
+													   @RequestBody Pokemon pokemon) {
+
+		return repository.findById(id).flatMap(existingPokemon -> {
+			existingPokemon.setNome(pokemon.getNome());
+			existingPokemon.setCategoria(pokemon.getCategoria());
+			existingPokemon.setHabilidade(pokemon.getHabilidade());
+			existingPokemon.setPeso(pokemon.getPeso());
+			return repository.save(existingPokemon);
+		}).map(updatePokemon -> ResponseEntity.ok(updatePokemon)).defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("{id")
+	public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable (value = "id") String id){
+		return repository.findById(id)
+					     .flatMap(existingPokemon -> repository.delete(existingPokemon)
+					    		 .then(Mono.just(
+					    				 ResponseEntity.ok().<Void>build())))
+					     .defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping
+	public Mono<Void> deleteAllPokemons (){
+		return repository.deleteAll();
 	}
 
 }
